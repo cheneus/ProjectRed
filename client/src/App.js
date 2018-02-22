0260e61d313b2f414159da9272cf7a5b47ac60a7
ChenESia <cheneus@gmail.com>
2018-02-22 17:15:14 -0600

    eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
-  state = {
-  	response: ''
-  };
+	state = {
+		response: ''
+	};
 
-  componentWillMount() {
-  	axios.get('/api/hello')
-  		.then(res => this.setState({ response: res.express }))
-  		.catch(err => console.log(err))
-  }
+	componentWillMount() {
+		axios.get('/api/hello')
+			.then(res => this.setState({ response: res.express }))
+			.catch(err => console.log(err))
+	}
 
-  callApi = () => {
-  	return 
-  };
+	callApi = () => {
+		return 
+	};
 
-  render() {
-  	return (
-  		<div className="App">
-  			<header className="App-header">
-  				<img src={logo} className="App-logo" alt="logo" />
-  				<h1 className="App-title">Welcome to React</h1>
-					<img src="./img/google-signIn.png" alt="sign-in" onClick={()=> {axios.get("/auth/google")}}/>
-  			</header>
-  			<p className="App-intro">
-  				{this.state.response}
-  			</p>
-  		</div>
-  	);
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+					<img src="./img/google-signIn.png" alt="sign-in" onClick={()=> {axios.get('/auth/google')}}/>
+				</header>
+				<p className="App-intro">
+					{this.state.response}
+				</p>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
