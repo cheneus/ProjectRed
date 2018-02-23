d858d3c9325e2bc6e192dd6850ceedea668953c7
ncloyd <ncloyd33@gmail.com>
2018-02-23 15:00:47 -0600

    Basic quiz is working and determining persontality result. Needs styling, back and next buttons, etc

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-3">TraveLlama</h1>
+  </div>
+</div>
+);
+
+export default NavBar;
---
+++ client/src/components/Quiz/quizQuestions.js
@@ -0,0 +1,175 @@
+var quizQuestions = [
+    
+    {
+        question: 'For your inflight entertainment which genre would you choose?',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content:'A Domedy'
+                    }, 
+                    {
+                        type: 'PersonB',
+                        content: 'A Documentary'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'An Action Movie'
+                    } 
+                ]
+    },
+    {
+        question: 'Your ideal Friday night consists of..',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Netflix'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'Drinks with your closest friends'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Out until the sun comes up'
+                    }
+                ]
+    },
+   {
+        question: 'While on vacation your priority is to..',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Relax'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'See as much of my destination as possible'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Try something I\'ve never done before'
+                    }
+                ]
+    },
+    {
+        question: 'Which best describes your perfect date?',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Dinner and a Movie'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'Browsing a local Art Show'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Cocktails and Dancing'
+                    }
+                ]
+    },
+        {
+        question: 'What could you never travel without?',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'A book'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'My camera'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Sneakers'
+                    }
+                ]
+    },
+     {
+        question: 'You prefer to spend time outdoors..',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Lounging in a hammock'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'Browsing a Farmers Market'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Hiking the nearest mountain'
+                    }
+                ]
+    },
+     {
+        question: 'Sunday mornings are for..',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Sleeping in'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'Brunch'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Spin Class'
+                    }
+                ]
+    },
+     {
+        question: 'Pick an item for your bucket list',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Watching the sunset over Santorini, Greece'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'Taste your way through Italy'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Hike Machu Picchu'
+                    }
+                ]
+    },
+     {
+        question: 'Looking through your vacation photos you will most likely see..',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'Shots of the beach at sunset'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'Close-ups of all the amazing food I ate'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'Clifftop selfies'
+                    }
+                ]
+    },
+     {
+        question: 'If you could dine with a historical figure who would you choose?',
+        answers: [
+                    {
+                        type: 'PersonA',
+                        content: 'My favorite Author'
+                    },
+                    {
+                        type: 'PersonB',
+                        content: 'A notorious celebrity'
+                    },
+                    {
+                        type: 'PersonC',
+                        content: 'A famous inventor'
+                    }
+                ]
+    }
+];
+
+export default quizQuestions;
