283b3b8cc16b8df89f0cdc5168397eb9bee96e1f
ncloyd <ncloyd33@gmail.com>
2018-03-02 19:42:17 -0600

    added react router

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import "./NavBar.css";
 const NavBar = () => (
 <div className="jumbotron jumbotron-fluid">
   <div className="container">
-    <h1 className="display-3">TraveLlama</h1>
+  	<h1 className="display-3">TraveLlama</h1>
   </div>
 </div>
 );
