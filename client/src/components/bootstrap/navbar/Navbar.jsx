e3f37930acf51d88628a8a6ce715ac55f273c918
ChenESia <cheneus@gmail.com>
2018-03-04 14:52:14 -0600

    navbar base completed

--- client/src/components/bootstrap/navbar/Navbar.jsx
+++ client/src/components/bootstrap/navbar/Navbar.jsx
@@ -1,33 +1,47 @@
 // Defining a stateless, functional component, MyNavLinks.
 // This component contains your navigation links.
-import React from 'react';
+import React from "react";
 // import AppBar from "material-ui/AppBar";
 // import FlatButton from "material-ui/FlatButton";
 // import ToolbarGroup from "material-ui/Toolbar/ToolbarGroup";
-import { Link } from 'react-router-dom';
-import Auth from '../../../modules/Auth';
-import './navbar.css';
+import { Link } from "react-router-dom";
+import Auth from "../../../modules/Auth";
+import "./navbar.css";
 // Another stateless, functional component, MyAppBar.
 // Here we are setting the iconElementRight property of Material UI's AppBar
 // to the component defined above.
 const NavBar = () => (
+  <nav class="navbar navbar-expand-lg">
+    <a className="navbar-brand" href="#">
+      TraveLlama
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+    <i class="material-icons">menu</i>
+    </button>
 
-  <nav className="jumbotron navbar navbar-expand-lg">
-    <a className="navbar-brand" href="#">TraveLlama</a>
-    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon" />
-  </button>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-    <ul className="navbar-nav ">
-        <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item active">
-            <a className="nav-link" href="/login">Login <span className="sr-only">(current)</span></a>
-          </li>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">
+            Home <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">
+            Login
+          </a>
+        </li>
       </ul>
-  </div>
-    </nav>
+    </div>
+  </nav>
 
   // <AppBar
   //   title="Travel"
