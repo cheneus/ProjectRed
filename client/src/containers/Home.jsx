e26077f69f7bc7f93a3e1f18612d06fdb588bf49
Yaeinn Park <yaeinnpark@gmail.com>
2018-03-08 21:19:00 -0600

    welcomepage

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -4,14 +4,13 @@ import {Link} from 'react-router-dom'
 
 
 const HomePage = () => (
-    <div className="back">
+   <div className="back">
     <img src="/img/mytraveLlama-no.png" alt="logo" className="wlogo img-fluid mx-auto d-block"/>
     <h1 className="display-3">Welcome to my traveLlama</h1>
-      <div className="takeQuiz">
-        <h2>Take our quiz to see your travel personality type and more!</h2>
-          <Link to="quiz" className='btn btn-primary quizbtn'>Take me to the quiz</Link>
-      </div>
-    </div>
+    <h2>Take our quiz to see your travel personality type and more!</h2>
+    <Link to="quiz" className='btn btn-primary quizbtn'>Take me to the quiz</Link>
+   </div>
+    
 );
 
 export default HomePage;
