9db057369574a861554ddaecc9303b292de12f2a
Yaeinn Park <yaeinnpark@gmail.com>
2018-03-08 20:18:20 -0600

    update homepage

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 
 const HomePage = () => (
     <div className="back">
-    <img src="/img/logo_resize.png" alt="logo" className="wlogo img-fluid mx-auto d-block"/>
+    <img src="/img/mytraveLlama-no.png" alt="logo" className="wlogo img-fluid mx-auto d-block"/>
     <h1 className="display-3">Welcome to my traveLlama</h1>
       <div className="takeQuiz">
         <h2>Take our quiz to see your travel personality type and more!</h2>
